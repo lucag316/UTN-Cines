a7172acff8d1c87e160f3e45c4431e82b8696e43
lucag316 <lucafranco316@gmail.com>
2024-10-16 19:09:16 -0300

    Updating promos

--- js/promociones.js
+++ js/promociones.js
@@ -1,18 +1,16 @@
-let currentIndex = 0; // Índice actual del carrusel
+let currentIndex = 0;
 
 function moveCarousel(carouselId, direction) {
     const carouselTrack = document.querySelector(`#${carouselId} .carousel-track`);
-    const items = carouselTrack.children; // Obtiene los elementos del carrusel
-    const itemWidth = items[0].clientWidth + 40; // Ancho del elemento + margen
+    const items = carouselTrack.children;
+    const totalItems = items.length;
+    const itemWidth = items[0].offsetWidth;
 
     if (direction === 'left') {
-        // Desplazarse a la izquierda
-        currentIndex = Math.max(currentIndex - 1, 0); // Asegura que no se mueva fuera del rango
-    } else if (direction === 'right') {
-        // Desplazarse a la derecha
-        currentIndex = Math.min(currentIndex + 1, items.length - 3); // Limita a la cantidad de elementos - 3
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+    } else {
+        currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
     }
 
-    // Aplica la transformación para desplazar el carrusel
     carouselTrack.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
 }
