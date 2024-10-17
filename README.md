1c0e41faa861d3c68807f65e1f5836c23c43e157
Ezequiel-Cura <ezequiel28cura@gmail.com>
2024-10-16 22:14:58 -0300

    pequeños cambios

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 Proyecto UTN pogramacion-3
+
+Puntos del Tp
+
+
+nav bar
+
+logo
+
+peliculas
+promociones
+
+carrito 
+usuario
+
+
+preguntar 
+
+carrito
+api/json
+modo administrador
+proceso checkout
+
+carrito
---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+
+let container_pelis = document.getElementById("#catalogo_pelis")
+
+let arrayPelis = fetch("https://api.themoviedb.org/3/movie/popular?api_key=f59f2a7d0dcf9beeeca9c90394385a92&language=es-ES&page=1")
+// 
