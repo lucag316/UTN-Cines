37eefecc7ffcd30e35f808266ac794aed566f24f
Ezequiel-Cura <ezequiel28cura@gmail.com>
2024-10-27 22:53:35 -0300

    fix % changes

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 
 
-let currentPage = 1;
 // URL base para la API de películas populares
+//  variables para el uso de la API
+let currentPage = 1;
 const API_KEY = "f59f2a7d0dcf9beeeca9c90394385a92";
 const BASE_URL ="https://api.themoviedb.org/3";
 const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=f59f2a7d0dcf9beeeca9c90394385a92&language=es-ES&page=";
 
+
+// Elementos que se usan para la interaccion con el DOM
 const container = document.getElementById('peliculas_container');
 const select_genero = document.querySelector("#select_genero");
+const select_orden = document.getElementById("select_orden");
 
-
+console.log(select_genero)
 // Función para hacer una solicitud a la API y obtener las películas
 async function fetchMovies(page = 1) {
     try {
-        const response = await fetch(`${API_URL}${page}`);
+        const response = await fetch(`${API_URL}${page}`); // Pedimos informacion a la api con la posicion de rama
         const data = await response.json();
         displayMovies(data.results);
         console.log(`Actual page: ${data.page}\nTotal pages:${data.total_pages}`)
@@ -91,7 +95,7 @@ function setupPagination(currentPage, totalPages) {
     });
 }
 
-const selectGenero = document.getElementById("select_genero");
+
 
 const cargarGeneros = async()=>{
     const res = await fetch("../datos.json");
@@ -102,7 +106,7 @@ const cargarGeneros = async()=>{
         const option = document.createElement("option");
         option.value = genero.id;
         option.textContent = genero.name;
-        selectGenero.appendChild(option);
+        select_genero.appendChild(option);
     });
 }
 
@@ -140,27 +144,27 @@ async function cargarPeliculas(generoId, orden, page = 1) {
 }
 
 // Manejar los eventos de cambio en los filtros
-document.getElementById("select_genero").addEventListener("change", function () {
+select_genero !== null ? select_genero.addEventListener("change", function () {
     const generoId = this.value;
     const orden = document.getElementById("select_orden").value;
     cargarPeliculas(generoId, orden);
-});
+}) : null;
 
-document.getElementById("select_orden").addEventListener("change", function () {
+select_orden !== null ?   select_orden.addEventListener("change", function () {
     const orden = this.value;
     const generoId = document.getElementById("select_genero").value;
     cargarPeliculas(generoId, orden);
-});
+}) : null;
 
 // Obtener las películas de la primera página al cargar la página
-document.addEventListener("DOMContentLoaded",async (event)=>{
-    try {
-        cargarGeneros()
-        fetchMovies();
-    } catch (error) {
-        console.log("Error al intentar conseguir las peliculas")
-    }
-});
+// document.addEventListener("DOMContentLoaded",async (event)=>{
+//     try {
+//         cargarGeneros()
+//         fetchMovies();
+//     } catch (error) {
+//         console.log("Error al intentar conseguir las peliculas")
+//     }
+// });
 
 
 // *!
@@ -250,83 +254,116 @@ document.addEventListener("DOMContentLoaded",async (event)=>{
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const apiKey = 'f59f2a7d0dcf9beeeca9c90394385a92';
-const language = 'es-ES';
 
-const main_tag = document.getElementById("perfil_peliculas-load")
-
-const img_perfil_peli = document.getElementById("img_perfil_peli");
-const iframe_pefil_peli = document.getElementById("iframe_perfil_peli");
-
-const titulo_text_tag = document.getElementById("titulo_text")
-const genero_text_tag = document.getElementById("genero_text")
-const duracion_text_tag = document.getElementById("duracion_text")
-const reparto_text_tag = document.getElementById("reparto_text")
-const director_text_tag = document.getElementById("director_text")
-const año_text_tag = document.getElementById("año_text")
-const resumen_text_tag = document.getElementById("resumen_text")
-
-// Función para obtener detalles de una película
-async function getMovieDetails(movieId) {
-    // Obtener detalles de la película (duración)
-    const detailsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=${language}`);
-    const details = await detailsResponse.json();
-
-    // Convertir el array de géneros en una lista de nombres
-    const genres = details.genres.map(genre => genre.name).join(', ');
-
-    // Obtener el reparto y el director
-    const creditsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=${language}`);
-    const credits = await creditsResponse.json();
-    const director = credits.crew.find(member => member.job === 'Director');
-    const cast = credits.cast.slice(0, 5).map(actor => actor.name); // Tomar los primeros 5 actores
-
-    // Obtener un video (tráiler)
-    const videosResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=${language}`);
-    const videos = await videosResponse.json();
-    const trailer = videos.results.find(video => video.type === 'Trailer');
-
-    // Mostrar resultados
-    console.log("ID:", details.id)
-    console.log("Título:", details.title);
-    console.log("Duración:", details.runtime, "minutos");
-    console.log("Director:", director ? director.name : "No disponible");
-    console.log("Reparto:", cast.join(", "));
-    console.log("Tráiler:", trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : "No disponible");
 
     
-    titulo_text_tag.textContent = details.title;
-    genero_text_tag.textContent = genres;
-    duracion_text_tag.textContent = details.runtime + " min";
-    reparto_text_tag.textContent = cast;
-    director_text_tag.textContent = director.name;
-    año_text_tag.textContent = details.release_date
-    resumen_text_tag.textContent = details.overview
-
-    img_perfil_peli.src = `https://image.tmdb.org/t/p/w500${details.poster_path}`
-    iframe_pefil_peli.src = `https://www.youtube.com/embed/${trailer.key}`;
-}
-
-// Función para obtener el ID de la película de la URL
-function getMovieIdFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('id');
-}
-
-
-document.addEventListener("DOMContentLoaded",(event)=>{
-    try {
-        const id_peli = getMovieIdFromUrl();
-
-        getMovieDetails(id_peli);
-    } catch (error) {
-        console.log("Error al intentar conseguir la pelicula")
+    const apiKey = 'f59f2a7d0dcf9beeeca9c90394385a92';
+    const language = 'es-ES';
+    
+    const main_tag = document.getElementById("perfil_peliculas-load")
+    
+    const img_perfil_peli = document.getElementById("img_perfil_peli");
+    const iframe_pefil_peli = document.getElementById("iframe_perfil_peli");
+    
+    const titulo_text_tag = document.getElementById("titulo_text")
+    const genero_text_tag = document.getElementById("genero_text")
+    const duracion_text_tag = document.getElementById("duracion_text")
+    const reparto_text_tag = document.getElementById("reparto_text")
+    const director_text_tag = document.getElementById("director_text")
+    const año_text_tag = document.getElementById("año_text")
+    const resumen_text_tag = document.getElementById("resumen_text")
+    
+    // Función para obtener detalles de una película
+    async function getMovieDetails(movieId) {
+        // Obtener detalles de la película (duración)
+        const detailsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=${language}`);
+        const details = await detailsResponse.json();
+    
+        // Convertir el array de géneros en una lista de nombres
+        const genres = details.genres.map(genre => genre.name).join(', ');
+    
+        // Obtener el reparto y el director
+        const creditsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=${language}`);
+        const credits = await creditsResponse.json();
+        const director = credits.crew.find(member => member.job === 'Director');
+        const cast = credits.cast.slice(0, 5).map(actor => actor.name); // Tomar los primeros 5 actores
+    
+        // Obtener un video (tráiler)
+        const videosResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=${language}`);
+        const videos = await videosResponse.json();
+        const trailer = videos.results.find(video => video.type === 'Trailer');
+    
+        // Mostrar resultados
+        console.log("ID:", details.id)
+        console.log("Título:", details.title);
+        console.log("Duración:", details.runtime, "minutos");
+        console.log("Director:", director ? director.name : "No disponible");
+        console.log("Reparto:", cast.join(", "));
+        console.log("Tráiler:", trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : "No disponible");
+    
+        
+        titulo_text_tag.textContent = details.title;
+        genero_text_tag.textContent = genres;
+        duracion_text_tag.textContent = details.runtime + " min";
+        reparto_text_tag.textContent = cast;
+        director_text_tag.textContent = director.name;
+        año_text_tag.textContent = details.release_date
+        resumen_text_tag.textContent = details.overview
+    
+        img_perfil_peli.src = `https://image.tmdb.org/t/p/w500${details.poster_path}`
+        iframe_pefil_peli.src = `https://www.youtube.com/embed/${trailer.key}`;
     }
-}) 
+    
+    // Función para obtener el ID de la película de la URL
+    function getMovieIdFromUrl() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('id');
+    }
 
 
 
 
+
+
+
+// document.addEventListener("DOMContentLoaded",(event)=>{
+//     try {
+//         const id_peli = getMovieIdFromUrl();
+
+//         getMovieDetails(id_peli);
+//     } catch (error) {
+//         console.log("Error al intentar conseguir la pelicula")
+//     }
+// }) 
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const path = window.location.pathname;
+    console.log(path)
+    // Para index.html
+    if (path.includes("index.html")) {
+        try {
+            console.log("Cargando películas en el catálogo...");
+            cargarGeneros()
+            fetchMovies();
+        } catch (error) {
+            console.log("Error al intentar conseguir las películas", error);
+        }
+    }
+
+    // Para perfil_peli.html
+    else if (path.includes("perfil_peli.html")) {
+        try {
+            const id_peli = getMovieIdFromUrl();
+            console.log("ID de la película:", id_peli);
+            getMovieDetails(id_peli);
+        } catch (error) {
+            console.log("Error al intentar conseguir los detalles de la película", error);
+        }
+    }
+});
 
 
 
