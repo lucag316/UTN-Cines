a54f2121b28fe590aaa897a4c3347c9b1c2da9f3
Ezequiel-Cura <ezequiel28cura@gmail.com>
2024-11-25 10:07:42 -0300

    carrito

---
+++ BACKEND/src/seeder/seeder.js
@@ -0,0 +1,23 @@
+ 
+const generoEndPointAPI = "https://api.themoviedb.org/3/genre/tv/list?api_key=f59f2a7d0dcf9beeeca9c90394385a92&language=es-ES"
+const pelisEndPointAPI = "https://api.themoviedb.org/3/movie/popular?api_key=f59f2a7d0dcf9beeeca9c90394385a92&language=es-ES&page=1"
+
+
+
+const generos = [
+    {
+
+    }
+]
+
+const reparto = [
+    {
+
+    }
+]
+
+const peliculas = [
+    {
+        
+    }
+]
