776ce065420e191239189a96088ce9ea7de3ca3b
Ezequiel-Cura <ezequiel28cura@gmail.com>
2024-12-05 11:14:53 -0300

    change .env

--- BACKEND/src/database.js
+++ BACKEND/src/database.js
@@ -4,16 +4,18 @@ const dotenv = require("dotenv");
 
 // Cargar variables de entorno
 dotenv.config();
+
+process.env.NODE_ENV= "production"
 // Determinar si estás en producción o en desarrollo
 const isProduction = process.env.NODE_ENV === "production";
 
 // Configuración de conexión a la base de datos
 const connection = mysql.createConnection({
-    host: isProduction ? "mysql.railway.internal" : process.env.DB_HOST,
-    database: isProduction ? "railway" : process.env.DB_DATABASE,
-    user: isProduction ? "root" : process.env.DB_USER,
-    password: isProduction ? "ijqYizWvBwrBzYEfrcCJXZIENFVMyPox" : process.env.DB_PASSWORD,
-    port: isProduction ? 3306 : process.env.DB_PORT || 3306
+    host: isProduction ? process.env.MYSQL_HOST : process.env.DB_HOST,
+    database: isProduction ? process.env.MYSQL_DATABASE : process.env.DB_DATABASE,
+    user: isProduction ? process.env.MYSQL_USER : process.env.DB_USER,
+    password: isProduction ? process.env.MYSQL_PASSWORD : process.env.DB_PASSWORD,
+    port: isProduction ? process.env.MYSQL_PORT : process.env.DB_PORT || 3306
 });
 
 // Función para obtener la conexión
