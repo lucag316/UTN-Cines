6065048f27ca3e7b583c2821d5a5f380ccab1cd0
Ezequiel-Cura <ezequiel28cura@gmail.com>
2024-12-07 16:40:59 -0300

    cambio URL para deploy

--- FRONTEND/js/peliculasService.js
+++ FRONTEND/js/peliculasService.js
@@ -1,5 +1,7 @@
+const API_BASE_URL = "https://utn-cines-production.up.railway.app"; // Cambia esto a la URL base de tu API
+
 async function getPeliculas() {
-    const res = await fetch("http://localhost:5000/AllPelis");
+    const res = await fetch(`${API_BASE_URL}/AllPelis`);
     const resJson = await res.json();
     return resJson;
 
